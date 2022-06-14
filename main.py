2e6963b5782a9bbc65c9e4a9abe8c1be05e1a1d4
Jacek Bakuński <jacek.bakunski@gmail.com>
2022-06-14 18:15:56 +0200

    Add library class

--- main.py
+++ main.py
@@ -1,45 +1,48 @@
-import random
 import sys
 import data
 import result_presentation
+from library import Library
 
-entire_library = data.feed_initial_data()
+library = Library(data.feed_initial_data())
 
-def search(library, title):
-    searched_items = []
-    for item in library:
-        if item.title.lower() == title:
-            searched_items.append(item)
-    return searched_items
+
+# def search(library, title):
+#     searched_items = []
+#     for item in library:
+#         if item.title.lower() == title:
+#             searched_items.append(item)
+#     return searched_items
     
 
-def generate_views(library):
-    choosed_item = random.choice(library)
-    choosed_item.number_of_plays += random.randrange(1, 100)
+# def generate_views(library):
+#     choosed_item = random.choice(library)
+#     choosed_item.number_of_plays += random.randrange(1, 100)
 
 
-def run_views_generator(library):
-    for x in range(10):
-        generate_views(library)
+# def run_views_generator(library):
+#     for x in range(10):
+#         generate_views(library)
 
 
-def top_titles(number_of_titles, content_type):
-    if content_type == 'movie':
-        movies = data.get_movies(entire_library)
-        sorted_movies = sorted(movies, key = lambda movie: movie.number_of_plays, reverse= True)
-        top_movies = sorted_movies[:number_of_titles]
-        return top_movies
-    elif content_type == 'series':
-        series = data.get_series(entire_library)
-        sorted_series = sorted(series, key = lambda series: series.number_of_plays, reverse=True)
-        top_series = sorted_series[:number_of_titles]
-        return top_series
+# def top_titles(number_of_titles, content_type):
+#     if content_type == 'movie':
+#         movies = data.get_movies(entire_library)
+#         sorted_movies = sorted(movies, key = lambda movie: movie.number_of_plays, reverse= True)
+#         top_movies = sorted_movies[:number_of_titles]
+#         return top_movies
+#     elif content_type == 'series':
+#         series = data.get_series(entire_library)
+#         sorted_series = sorted(series, key = lambda series: series.number_of_plays, reverse=True)
+#         top_series = sorted_series[:number_of_titles]
+#         return top_series
  
 
 if __name__ == "__main__":
     if len(sys.argv) < 2:
         exit(1)
-    run_views_generator(entire_library)
+    library.run_views_generator()
+    # run_views_generator(entire_library)
     movies_num = int(sys.argv[1])
     series_num = int(sys.argv[2])
-    result_presentation.display_info(top_titles, movies_num, series_num)
+    # result_presentation.display_info(top_titles, movies_num, series_num)
+    result_presentation.display_info(library.top_titles, movies_num, series_num)
